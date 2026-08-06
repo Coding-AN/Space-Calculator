e7f2983afb4a79ec24a9960096869dafd26ef24e
Aarav Nair <224329735+Aario-Wii@users.noreply.github.com>
2026-08-06 22:32:16 +0530

    Modification: Added Calculator Features like calculations, keybindings, and working buttons

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,42 +1,68 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using HarfBuzzSharp;
 
 namespace CalculatorGUI.ViewModels;
 
 //Add A Theme Toggle, Calculation History Storage, Unit Conversions, and Keyboard Support
 public partial class MainViewModel : ViewModelBase
 {
-   
-}
+   [ObservableProperty]
+   public partial string Input {get; set;} = "";
 
-public partial class CalcViewModel :ViewModelBase
-{
-    private double firstValue = 0;
-    private double secondValue = 0;
-    private bool OperationSelected = false;
-    private enum Operations
+    [RelayCommand]
+    public void Calculate()
     {
-        add,
-        subtract,
-        multiply,
-        divide
-    }
-
-    public double SelectedValue
-    {
-        get
+        //Fix weird errors like 14/2 = 0.5
+        try
         {
-            if(OperationSelected)
+            int operand = Input.IndexOfAny(new char[]{'+', '-', '×', '÷', '*', '/'});
+            float num1 = float.Parse(Input.Substring(0,Input.Length - operand - 1));
+            float num2 = float.Parse(Input.Substring(operand + 1));
+            switch(Input[operand])
             {
-                return secondValue;
-            }
-            else
-            {
-                return firstValue;
+                case '+' :
+                    Input = (num1 + num2).ToString();
+                    break;
+                case '-':
+                    Input = (num1 - num2).ToString();
+                    break;
+                case '×':
+                case '*':
+                    Input = (num1 * num2).ToString();
+                    break;
+                case '÷':
+                case '/':
+                    Input = (num1 / num2).ToString();
+                    break;
             }
         }
-        set;
-        
+        catch{}
+    }
+
+    [RelayCommand]
+    public void AddDigit(string content)
+    {
+        //Make it work 100% of the time
+        if(content.IndexOfAny(new char[]{'+', '-', '×', '÷'}) != -1 && Input.IndexOfAny(new char[]{'+', '-', '×', '÷', '*', '/'}) != -1)
+        {
+            Calculate();
+        }
+        Input += content;
+    }
+
+    [RelayCommand]
+    public void RemoveDigit()
+    {
+        if(Input.Length >0)
+        {
+            Input = Input!.Substring(0,Input.Length-1);
+        }
+    }
+
+    [RelayCommand]
+    public void ClearInput()
+    {
+        Input = "";
     }
 }
