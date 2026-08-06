f32de27d816b9f37c3baa806e235a5ed89bc6303
Aarav Nair <224329735+Aario-Wii@users.noreply.github.com>
2026-08-06 13:28:39 +0530

    Initial Commit

---
+++ Models/CalcFunctions.cs
@@ -0,0 +1,14 @@
+namespace CalculatorGUI.Models;
+
+public class CalcView
+{
+    public double firstValue = 0;
+    public double secondValue = 0;
+    public enum Operations
+    {
+        add,
+        subtract,
+        multiply,
+        divide
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CalculatorGUI.Models;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CalculatorGUI.ViewModels;
+
+//Add A Theme Toggle, Calculation History Storage, Unit Conversions, and Keyboard Support
+public partial class MainViewModel : ViewModelBase
+{
+   
+}
+
+public partial class CalViewModel :ViewModelBase
+{
+    
+}
